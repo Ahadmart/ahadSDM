f521981a50e0dc286f6d01460b3413a51940437a
abumuhammad <b.hermianto@gmail.com>
2017-08-14 09:03:59 +0700

    Database Config

---
+++ protected/config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+// This is the database connection configuration.
+return [
+    'connectionString' => 'mysql:host=localhost;dbname=ahadsdm',
+    'emulatePrepare' => true,
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
